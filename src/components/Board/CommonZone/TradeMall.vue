4e30bbdcc5f1f5ae8a4b884bbe3c0cd2afad15ac
Abramenko Svetlana <abramenko.svetlana@mail.ru>
2023-03-09 21:57:38 +0300

    Набросок объектов игрового стола

---
+++ src/components/Board/CommonZone/TradeMall.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <div class="row">
+        <div class="col">
+            Deck component
+        </div>
+    </div>
+</template>
+
+<script>
+
+// Code here will be called once.
+
+export default {
+    setup() {
+        // Will be called on instance creation.
+    }
+}
+</script>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+
+}
+</style>
